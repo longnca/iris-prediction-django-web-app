fb4b452ee170c627190a69a8dec45be4d53e139c
longbytes <dsbuff01@gmail.com>
2024-03-17 18:36:40 -0600

    add class Meta in models.py to prevent 'resultss' for plural names and add screenshots

--- iris_predict_app/models.py
+++ iris_predict_app/models.py
@@ -9,5 +9,9 @@ class PredResults(models.Model):
     petal_width = models.FloatField()
     classification = models.CharField(max_length=30)
 
+    class Meta:
+        """Tells Django how to use plural names of Pred Results."""
+        verbose_name_plural = 'Pred results'
+
     def __str__(self):
         return self.classification
